b60affa3c17d542ca0726d060cab889a91701184
Hamza <olowuhamzat@gmail.com>
2025-06-08 15:03:14 -0400

    Refactor S3 upload functionality in run_ingest.py to use DataFrame directly and streamline data handling

--- tax_bracket_ingest/run_ingest.py
+++ tax_bracket_ingest/run_ingest.py
@@ -1,41 +1,45 @@
+from io import BytesIO
 import os
 import boto3
-import tempfile
 from dotenv import load_dotenv
-from pathlib import Path
-from datetime import datetime
 
 import pandas as pd
 from tax_bracket_ingest.scraper.fetch import fetch_irs_data
 from tax_bracket_ingest.parser.parser import parse_irs_data, parse_irs_data_to_dataframe
-from tax_bracket_ingest.parser.normalize import process_irs_dataframe, dataframe_to_csv
+from tax_bracket_ingest.parser.normalize import process_irs_dataframe
 
 load_dotenv()
+DATA_PATH = os.getenv("DATA_PATH")
 S3_BUCKET = os.getenv("S3_BUCKET")
 S3_PREFIX = os.getenv("S3_PREFIX")
-
-def upload_to_s3(local_path: Path, s3_key: str):
+S3_KEY = os.getenv("S3_KEY")
+    
+def read_csv_from_s3(key: str) -> pd.DataFrame:
     s3 = boto3.client("s3")
-    s3.upload_file(str(local_path), S3_BUCKET, s3_key)
-    print(f"Uploaded {local_path.name} to s3://{S3_BUCKET}/{s3_key}")
+    resp = s3.get_object(Bucket=S3_BUCKET, Key=key)
+    return pd.read_csv(BytesIO(resp["Body"].read()))
+
+def write_df_to_s3(df: pd.DataFrame, key: str):
+    s3 = boto3.client("s3")
+    buf = BytesIO()
+    df.to_csv(buf, index=False)
+    buf.seek(0)
+    s3.put_object(Bucket=S3_BUCKET, Key=key, Body=buf.getvalue())
     
 def main():
     html = fetch_irs_data()
     
     raw_struct = parse_irs_data(html)
     raw_df = parse_irs_data_to_dataframe(raw_struct)
-    wide_df = process_irs_dataframe(raw_df)
+    curr_df = process_irs_dataframe(raw_df)
     
-    year = wide_df['Year'][0]
+    prev_hist = read_csv_from_s3(S3_KEY)
     
-    print(wide_df)
+    hist_df = pd.concat([curr_df, prev_hist], ignore_index=True)
     
-    with tempfile.TemporaryDirectory() as td:
-        fn = f"brackets_{year}.csv"
-        out = Path(td) / fn
-        dataframe_to_csv(wide_df, str(out))
-        print(out, fn)
-        # upload_to_s3(out, f"{S3_PREFIX}{fn}")
+    write_df_to_s3(hist_df, S3_KEY)
+    
+    print(f"Historical CSV at s3://{S3_BUCKET}/{S3_KEY} updated")
 
 if __name__ == "__main__":
     main()
